e371c79ed39307ef7391b9d7ef3e762a956df095
Joseph Lenton <josephlenton@studiofortress.com>
2012-07-26 13:15:04 +0100

    converted line endings from Windows to Unix

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,8 @@
 @echo off
 
-copy /B .\..\license.txt ..\dist\quby.js
-
-type ..\src\lib\*.js >> ..\dist\quby.js
-type ..\src\*.js >> ..\dist\quby.js
+type .\..\license.txt >  ..\dist\quby.js
+type ..\src\lib\*.js  >> ..\dist\quby.js
+type ..\src\*.js      >> ..\dist\quby.js
 
 @echo on
 rem ### Build Done! ###
